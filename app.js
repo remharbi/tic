70c01c652f80a914524205b2e8ccd261723a2d92
outsiderem <reemharbi.3@gmail.com>
2019-07-03 15:59:10 +0300

    Fixed scores and added localStorage

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 // My JS File
 
-var playerOneScore = 0;
-var playerTwoScore = 0;
-var tie = 0;
-window.sessionStorage.setItem("tie", tie);
-var turns = 0;
+let playerOneScore = 0;
+let playerTwoScore = 0;
+let tie = 0;
+let turns = 0;
 console.log("Game Started!");
 document.getElementById("p1Score").innerText = playerOneScore;
 document.getElementById("p2Score").innerText = playerTwoScore;
@@ -16,37 +15,16 @@ let playerTurn = [1, 2].sort((a, b) => {
     return Math.random() - 0.5;
 })
 
- // Run on page load
-  //   window.onload = function() {
-
-  //       // If sessionStorage is storing default values, exit the function and do not restore data
-  //       if (sessionStorage.getItem('playerOneScore') == 0 && sessionStorage.getItem('playerTwoScore') == 0 && sessionStorage.getItem('tie') == 0 ) {
-  //           return;
-  //       } else {
-  //       // If values are not blank, restore them to the fields
-  //       var playerOneScore = sessionStorage.getItem('playerOneScore');
-  //       if (playerOneScore !== null) $('#p1Score').val(p1Score);
-  //       console.log(playerOneScore);
-
-  //       // var email = sessionStorage.getItem('email');
-  //       // if (email !== null) $('#inputEmail').val(email);
-
-  //       // var subject= sessionStorage.getItem('subject');
-  //       // if (subject!== null) $('#inputSubject').val(subject);
-
-  //       // var message= sessionStorage.getItem('message');
-  //       // if (message!== null) $('#inputMessage').val(message);
-  //   }
-  // }
-
-  //   // Before refreshing the page, save the form data to sessionStorage
-  //   window.onbeforeunload = function() {
-  //       sessionStorage.setItem("p1Score", $('#p1Score').val());
-  //       // sessionStorage.setItem("email", $('#inputEmail').val());
-  //       // sessionStorage.setItem("subject", $('#inputSubject').val());
-  //       // sessionStorage.setItem("message", $('#inputMessage').val());
-  //   }
-
+ //Run on page load
+    window.onload = function() {
+      if(localStorage.getItem("p1Score") !== null && localStorage.getItem("p2Score") !== null) {
+        playerOneScore = parseInt(localStorage.getItem("p1Score"));
+        playerTwoScore = parseInt(localStorage.getItem("p2Score"));
+        document.getElementById("p1Score").innerText = playerOneScore;
+        document.getElementById("p2Score").innerText = playerTwoScore;    
+        }    
+      console.log(playerOneScore);
+    }
 
 const turnIndicator = function(){
 	if (playerTurn[0] == 1) {
@@ -61,14 +39,7 @@ const turnIndicator = function(){
 }
 
 const refresh = function() {
-    turns = 0;
-    $(".col").on('click', startGame);
-    $(".col").css({"background-image": "none"});
-    $(".col").removeClass("p1");
-    $(".col").removeClass("p1");
-  console.log(playerOneScore);
-  console.log(playerTwoScore);
-  // window.location.reload();
+  window.location.reload();
   }
 
 $(".replay").click(function(){
@@ -108,24 +79,25 @@ let boxes = document.querySelectorAll('.col');
 function testWin(a,b,c){
   var win = false;
  	if (a.classList.contains("p1") && b.classList.contains("p1") && c.classList.contains("p1")){
-      // localStorage.setItem(p1Score, playerOneScore);
+    
       swal("Player 1 won !!!", { button: "retry", className: "p1Win", closeOnClickOutside: false}).then(refresh);
       playerOneScore+=1;
+      console.log("p1Score", playerOneScore)
+      localStorage.setItem("p1Score", playerOneScore);
       console.log(playerOneScore);
-      updateScores();
       win = true;
+      updateScores();
   } else if (a.classList.contains("p2") && b.classList.contains("p2") && c.classList.contains("p2")){
       console.log("Player 2 wins!");
-      // localStorage.setItem(p2Score, playerTwoScore);
       swal("Player 2 won !!!", { button: "retry", closeOnClickOutside: false, }).then(refresh);
       playerTwoScore+=1;
-      updateScores();
+      console.log("p2Score", playerTwoScore)
+      localStorage.setItem("p2Score", playerTwoScore);
       win = true;
+      updateScores();
 } else if (turns == 9 && !win){
 			console.log("it's a tie!");
-			//localStorage.setItem(tie, tie);
       swal("It's a Tie.", { button: "retry", closeOnClickOutside: false, }).then(refresh);
-      tie+=1; 
     }
 }
 
@@ -169,9 +141,3 @@ testWin(boxes[2],boxes[4],boxes[6]);
 startGame();
 
 
-	// var Coin = $(this).attr('id'); //fileName as id button
-   //  var audiot = new Audio("file/" +Coin+".mp3");
-   //  audiot.play();
-  
-
-  //DOMContentLoaded
